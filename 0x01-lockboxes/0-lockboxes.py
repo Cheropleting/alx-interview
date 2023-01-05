93ef436b0f42ce2e94674a680fc1b26065c2b39f
devbxtzz <batztonnie@gmail.com>
2023-01-05 10:02:50 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
 """
-Module 0-lockboxes
+    This module contains the canUnlockAll function.
 """
 
 
 def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened
+        Arg:
+            boxes: Is a list of lists.
     """
-    You have n number of locked boxes in front of you.
-    Each box is numbered sequentially from 0 to n - 1,
-    and each box may contain keys to the other boxes.
-    Determine if all the boxes can be opened.
-    """
-    if boxes is None:
-        return False
-    if len(boxes) == 1:
-        return True
-    # Track the visited boxes
-    visited = set()
-    # The first box is unlocked
-    visited.add(0)
+    box = len(boxes) * [False]
+    box[0] = True
+    keys = [0]
 
-    # Use a stack to do a Depth First Search
-    stack = []
-    stack.append(0)
-
-    while stack:
-        keys = boxes[stack.pop()]
-        for key in keys:
-            if key not in visited:
-                visited.add(key)
-                stack.append(key)
-    # All have been visited if the
-    # number of boxes is equal to the
-    # number of visited boxes
-    return len(boxes) == len(visited)
+    for each in keys:
+        for i in boxes[each]:
+            if i not in keys:
+                if i < len(boxes):
+                    box[i] = True
+                    keys.append(i)
+    return all(box)
